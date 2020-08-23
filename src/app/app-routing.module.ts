0050b616fb66a9f1b623b6e817b8708a81824119
s.sarveswararao <46348653+sarvesvararaos@users.noreply.github.com>
2020-08-23 22:26:45 +0530

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
